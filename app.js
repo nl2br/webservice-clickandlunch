05acaf5c05a2d2e4a0deb53015b141b7ba084be4
nathan <natleb7@gmail.com>
2018-11-18 16:04:26 +0100

    ajout de nodemon

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req,res) => {
     // process.exit(); // stop the server
     res.setHeader('Content-type', 'text/html');
-    res.write(`<html><body><h1>hello</h1></body></html>`);
+    res.write(`<html><body><h1>hello 1</h1></body></html>`);
     res.end();
 });
 
