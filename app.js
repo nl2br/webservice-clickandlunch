fba1658b5b846db201fba4bb1bb3aa1caa8499db
nathan <natleb7@gmail.com>
2018-11-24 01:44:43 +0100

    Ajout du module JOI et verifications des inputs

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -25,9 +26,16 @@ app.get('/api/dishes/:id', (req, res) => {
 });
 
 app.post('/api/dishes', (req, res) => {
-    if(!req.body.name || req.body.name < 3){
-        res.status(400).send('Name is required and have more 3 caracters');
+    const result = validateDishe(req.body)
+    if(result.error){
+        let errors = '';
+        for(let err of result.error.details){
+            errors = err.message + ' ';
+        }
+        res.status(400).send(errors);
+        return;
     }
+
     const dishe = {
         id: dishes.length + 1,
         name: req.body.name
@@ -35,6 +43,31 @@ app.post('/api/dishes', (req, res) => {
     dishes.push(dishe);
     res.send(dishe);
 });
+
+app.put('/api/dishes/:id', (req, res) => {
+    const dishe = dishes.find(d => d.id === parseInt(req.params.id));
+    if(!dishe) res.status(404).send("this ID dishe don't exist");
+
+    const result = validateDishe(req.body)
+    if(result.error){
+        let errors = '';
+        for(let err of result.error.details){
+            errors = err.message + ' ';
+        }
+        res.status(400).send(errors);
+        return;
+    }
+
+    dishe.name = req.body.name;
+    res.send(dishe);
+});
+
+function validateDishe(dishe) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(dishe, schema);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
