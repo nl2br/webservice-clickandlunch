b86ffb3037e26170c473980f75ddbd87390ef8fb
nathan <natleb7@gmail.com>
2018-12-02 11:11:23 +0100

    tab4 to tab2

--- routes/dishes.js
+++ routes/dishes.js
@@ -8,68 +8,68 @@ const Joi = require('joi');
 const disheController = require('../controllers/disheController');
 
 // GET /dishes Request for list of all Dishe items.
-router.get('/', disheController.getAllDishes );
+router.get('/', disheController.getAllDishes);
 
 
 router.get('/:id', (req, res) => {
-    let dishe = isIdDisheExist(parseInt(req.params.id))
-    if(!dishe) return res.status(404).send("this ID dishe don't exist");
-    res.send(dishe);
+  let dishe = isIdDisheExist(parseInt(req.params.id))
+  if (!dishe) return res.status(404).send("this ID dishe don't exist");
+  res.send(dishe);
 });
 
 router.post('/', (req, res) => {
-    const result = validateDishe(req.body)
-    if(result.error){
-        let errors = '';
-        for(let err of result.error.details){
-            errors = err.message + ' ';
-        }
-        return res.status(400).send(errors);
+  const result = validateDishe(req.body)
+  if (result.error) {
+    let errors = '';
+    for (let err of result.error.details) {
+      errors = err.message + ' ';
     }
-    const dishe = {
-        id: dishes.length + 1,
-        name: req.body.name
-    };
-    dishes.push(dishe);
-    res.send(dishe);
+    return res.status(400).send(errors);
+  }
+  const dishe = {
+    id: dishes.length + 1,
+    name: req.body.name
+  };
+  dishes.push(dishe);
+  res.send(dishe);
 });
 
 router.put('/:id', (req, res) => {
-    let dishe = isIdDisheExist(parseInt(req.params.id))
-    if(!dishe) return res.status(404).send("this ID dishe don't exist");
+  let dishe = isIdDisheExist(parseInt(req.params.id))
+  if (!dishe) return res.status(404).send("this ID dishe don't exist");
 
-    const result = validateDishe(req.body)
-    if(result.error){
-        let errors = '';
-        for(let err of result.error.details){
-            errors = err.message + ' ';
-        }
-        return res.status(400).send(errors);
+  const result = validateDishe(req.body)
+  if (result.error) {
+    let errors = '';
+    for (let err of result.error.details) {
+      errors = err.message + ' ';
     }
+    return res.status(400).send(errors);
+  }
 
-    dishe.name = req.body.name;
-    res.send(dishe);
+  dishe.name = req.body.name;
+  res.send(dishe);
 });
 
-router.delete('/:id',(req, res) => {
-    let dishe = isIdDisheExist(parseInt(req.params.id))
-    if(!dishe) return res.status(404).send("this ID dishe don't exist");
+router.delete('/:id', (req, res) => {
+  let dishe = isIdDisheExist(parseInt(req.params.id))
+  if (!dishe) return res.status(404).send("this ID dishe don't exist");
 
-    const index = dishes.indexOf(dishe);
-    dishes.splice(index,1);
+  const index = dishes.indexOf(dishe);
+  dishes.splice(index, 1);
 
-    res.send(dishe);
+  res.send(dishe);
 });
 
 function validateDishe(dishe) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-    return Joi.validate(dishe, schema);
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+  return Joi.validate(dishe, schema);
 }
 
 function isIdDisheExist(id) {
-    return dishes.find(d => d.id === id);
+  return dishes.find(d => d.id === id);
 }
 
 module.exports = router;
