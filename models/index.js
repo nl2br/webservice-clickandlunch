59115ce18a2de1048524eaac2a77caf6d20e9698
nathan <natleb7@gmail.com>
2019-03-09 14:39:12 +0100

    to postgree

--- models/index.js
+++ models/index.js
@@ -10,24 +10,31 @@ const db = {};
 // let logConsoleSequelize = process.env.NODE_ENV === 'test' ? true : false;
 let logConsoleSequelize = process.env.NODE_ENV === 'test' ? false : false;
 
-// TODO: faire un bon fichier de config avec les var_ENV
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  {
-    dialect: config.dialect,
-    host: config.host,
-    port: config.port,
-    define: { 
-      timestamps: true, 
-      freezeTableName: true, 
-      underscored: false
-    },
-    timezone: 'Europe/Paris',
-    logging: logConsoleSequelize
-  }
-);
+if(config.use_env_variable){
+  const sequelize = new Sequelize(
+    process.env[config.use_env_variable],
+    config,
+  );
+}else{
+  const sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    {
+      dialect: config.dialect,
+      host: config.host,
+      port: config.port,
+      define: { 
+        timestamps: true, 
+        freezeTableName: true, 
+        underscored: false
+      },
+      timezone: 'Europe/Paris',
+      logging: logConsoleSequelize
+    }
+  );
+}
+
 console.log(`ENV to "${process.env.NODE_ENV}"`);
 console.log(`Connected to "${config.database}" database`);
 
